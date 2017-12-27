c1d46628e84a837f2491be8454292dafb6d0d85c
srinivasarao <srinivasaraouesi@gmail.com>
2017-12-27 19:24:12 +0530

    first commit

---
+++ app/src/main/java/com/srinu/sqlitecurd/User.java
@@ -0,0 +1,46 @@
+package com.srinu.sqlitecurd;
+
+/**
+ * Created by ConquerorsTechnologies on 20/12/17.
+ */
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
